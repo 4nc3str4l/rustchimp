28f948f55985ad0e6123597d99a25f293713b119
4nc3srtr4l <cmuri_10@hotmail.com>
2022-12-07 19:58:54 +0100

    Refactor code to make it easier to be tested

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use rustchimp::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    run()?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,11 @@
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000")
+        .get(r#"http://127.0.0.1:8888/health_check"#)
         .send()
         .await
         .expect("Failed to execute request");
@@ -15,6 +15,7 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background
-async fn spawn_app() -> std::io::Result<()> {
-    rustchimp::run().await
+fn spawn_app() {
+    let server = rustchimp::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }
