51291241ecc8eb67f4d39cd6adae33ad72c6a76f
4nc3srtr4l <cmuri_10@hotmail.com>
2022-12-07 20:39:43 +0100

    Try to make tests run in a free port all the time

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,6 +16,7 @@ async fn health_check_works() {
 
 // Launch our application in the background
 fn spawn_app() {
-    let server = rustchimp::run().expect("Failed to bind address");
+    // Port 0 makes the operative system to scan for a free port
+    let server = rustchimp::run("127.0.0.1:0").expect("Failed to bind address");
     let _ = tokio::spawn(server);
 }
